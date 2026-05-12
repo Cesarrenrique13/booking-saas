c7bb35ea2da54825c13ea3a0614a8d40350b02d0
cesarrenqieque13 <cs2211058@gmail.com>
2026-05-11 21:56:09 -0400

    businesses repository

---
+++ src/businesses/repositories/business.repository.ts
@@ -0,0 +1,65 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Business } from '../entities/business.entity';
+import { DeleteResult, Repository } from 'typeorm';
+import { validate as uuidValidate } from 'uuid';
+
+@Injectable()
+export class BusinessRepository {
+  constructor(
+    @InjectRepository(Business)
+    private readonly businessRepository: Repository<Business>,
+  ) {}
+
+  async createBusiness(data: Business): Promise<Business> {
+    const business = this.businessRepository.create(data);
+    return this.businessRepository.save(business);
+  }
+
+  async findBusinesses(
+    skip: number,
+    take: number,
+  ): Promise<[Business[], number]> {
+    return this.businessRepository.findAndCount({
+      skip,
+      take,
+      relations: { owner: true },
+      where: { isActive: true },
+      order: {
+        createdAt: 'DESC',
+      },
+    });
+  }
+
+  async findBusiness(term: string): Promise<Business | null> {
+    let business: Business | null;
+
+    if (uuidValidate(term)) {
+      business = await this.businessRepository.findOne({
+        where: { id: term, isActive: true },
+        relations: { owner: true },
+      });
+    } else {
+      const queryBuilder =
+        this.businessRepository.createQueryBuilder('business');
+      business = await queryBuilder
+        .leftJoinAndSelect('business.owner', 'owner')
+        .where('business.slug ILIKE :slug', { slug: term })
+        .andWhere('business.isActive = :isActive', { isActive: true })
+        .getOne();
+    }
+    return business;
+  }
+
+  async updateBusiness(id: string, data: Partial<Business>): Promise<Business> {
+    const business = await this.businessRepository.preload({ id, ...data });
+    if (!business) {
+      throw new NotFoundException('Business not found');
+    }
+    return this.businessRepository.save(business);
+  }
+
+  async deleteBusiness(id: string): Promise<DeleteResult> {
+    return this.businessRepository.softDelete(id);
+  }
+}
